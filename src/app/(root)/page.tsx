22f324aee7e8323b47219fa6da11faab4786b09f
Mohamed Ali Nasr <mohamedalinasr4444@gmail.com>
2024-02-09 18:20:40 +0200

    finish the app

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
 import { SearchParamProps } from "@/types/actions";
@@ -55,8 +57,8 @@ const Home = async ({ searchParams }: SearchParamProps) => {
         </h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          {/* <Search />
-          <CategoryFilter /> */}
+          <Search />
+          <CategoryFilter />
         </div>
 
         <Collection
